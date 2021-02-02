864c889b1d2e1887586eee3462f340afa4905f67
milanwinter <milanwinter8@gmail.com>
2021-02-01 22:22:31 -0800

    commit tree 7555c932153f4dd6310e0b66219b12dfbcf8f694

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+import React, {Component} from 'react'
+
+class Bomb extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+
+    render() {
+        return (
+          this.state.secondsLeft != 0 ?  <p>{this.state.secondsLeft} seconds left before I go boom!</p> : <p>Boom!</p>
+        )
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,25 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+
+class ImageSlider extends React.Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+
+
+
+    render() {
+        return (
+            <p>I am on slide {this.state.currentSlideIndex}</p>
+        )
+    }
+}
+
+export default ImageSlider  
